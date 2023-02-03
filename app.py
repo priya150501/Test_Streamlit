826ca3c59a97dec5b1c3b48a908b6db704fa742f
Priya <123946200+priya150501@users.noreply.github.com>
2023-02-03 17:17:26 +0530

    Added calulator function

--- app.py
+++ app.py
@@ -6,6 +6,42 @@ import streamlit as st
 st.title("Welcome To Calculator")
 
 
+# Text Input
+
+# save the input text in the variable 'name'
+# first argument shows the title of the text input box
+# second argument displays a default text inside the text input area
+name = st.text_input("Enter First Number", "Type Here ...")
+name = st.text_input("Enter Second Number", "Type Here ...")
+
+# display the name when the submit button is clicked
+# .title() is used to get the input text string
+if(st.button('Submit')):
+	result = name.title()
+	st.success(result)
+
+
+def cal():
+	if(operation == 'Addition'):
+		result = var_1 + var_2
+	elif(operation == 'Subtraction'):
+		result = var_1 - var_2
+	elif(operation == 'Multiplication'):
+		result = var_1 * var_2
+	elif(operation == 'Division'):
+		if(var_1==0 and var_2==0):
+			result = 0
+		else:
+			result = var_1 / var_2
+	else:
+		result = 0
+
+st.success(f"Answer = {ans}")
+  
+if st.button("Calculate result"):
+    calculate()
+
+	
 # import module
 import streamlit as st
 
