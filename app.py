916311742771723d09c63accf76591e3fc7ee452
Priya <123946200+priya150501@users.noreply.github.com>
2023-02-03 17:18:56 +0530

    added cal function

--- app.py
+++ app.py
@@ -14,6 +14,22 @@ st.title("Welcome To Calculator")
 name = st.text_input("Enter First Number", "Type Here ...")
 name = st.text_input("Enter Second Number", "Type Here ...")
 
+
+# radio button
+# first argument is the title of the radio button
+# second argument is the options for the radio button
+status = st.radio("Select Operator: ", ('Adddition', 'Subtraction','Multiplication','Division'))
+
+# conditional statement to print
+# show the result using the success function
+if (status == 'Addition'):
+	st.success("Addition")
+elif(status == 'Subtraction'):
+	st.success("Subtraction")
+elif(status == 'Multiplication'):
+	st.success("Multiplication")
+elif(status == 'Division'):
+	st.success("Division")
 # display the name when the submit button is clicked
 # .title() is used to get the input text string
 if(st.button('Submit')):
@@ -41,22 +57,4 @@ st.success(f"Answer = {ans}")
 if st.button("Calculate result"):
     calculate()
 
-	
-# import module
-import streamlit as st
 
-# radio button
-# first argument is the title of the radio button
-# second argument is the options for the radio button
-status = st.radio("Select Operator: ", ('Adddition', 'Subtraction','Multiplication','Division'))
-
-# conditional statement to print
-# show the result using the success function
-if (status == 'Addition'):
-	st.success("Addition")
-elif(status == 'Subtraction'):
-	st.success("Subtraction")
-elif(status == 'Multiplication'):
-	st.success("Multiplication")
-elif(status == 'Division'):
-	st.success("Division")
