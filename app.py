581b3d45cfec12ffacaf5621749ac12f70f23a1c
Priya <123946200+priya150501@users.noreply.github.com>
2023-02-03 16:45:01 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
 
+# import module
+import streamlit as st
+
+# Title
+st.title("Hello GeeksForGeeks !!!")
+
+# success
+st.success("Success")
+
+# success
+st.info("Information")
+
+# success
+st.warning("Warning")
+
+# success
+st.error("Error")
+
+# Exception - This has been added later
+exp = ZeroDivisionError("Trying to divide by Zero")
+st.execption(exp)
